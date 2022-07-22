7ae7b4b67d572fa3a14777cb1137bd648c233f0a
James Mete <jmete3@gmail.com>
2022-07-22 14:56:54 +0000

    Removing sendgrid. Transitioned to SES.

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,9 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 var sanitizer = require('sanitizer');
-const sgMail = require('@sendgrid/mail')
 const AWS = require('aws-sdk');
 
 dotenv.config();
-
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 AWS.config.update({
     region: process.env.AWS_SES_REGION,
@@ -28,37 +25,6 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
-app.post('/api/email', (req, res) => {
-    const nameValue = sanitizer.sanitize(req.body.name);
-    const emailValue = sanitizer.sanitize(req.body.email);
-    const messageValue = sanitizer.sanitize(req.body.message);
-
-    sgMail.send({
-        from: process.env.FROM_EMAIL,
-        to: process.env.TO_EMAIL,
-        subject: "James Website Contact Form",
-        html: `
-        <h1>Name:</h1>
-        <p>${nameValue}</p>
-        <h1>Email:</h1>
-        <p>${emailValue}</p>
-        <h1>Message:</h1>
-        <p>${messageValue}</p>
-        `
-    },
-    (error,body) => {
-        if (error) {
-            console.log(error);
-            res.status(500).send({message: "Error in sending the email"})
-        } else {
-            console.log(body);
-            res.send({message: "Email sent successfully"})
-        }
-    }
-
-    );
-});
 
 const sendEmail = (res,message, from) => {
     const params = {
@@ -92,7 +58,7 @@ const sendEmail = (res,message, from) => {
     });
 };
 
-app.post('/api/emailses', (req, res) => {
+app.post('/api/email', (req, res) => {
     const nameValue = sanitizer.sanitize(req.body.name);
     const emailValue = sanitizer.sanitize(req.body.email);
     const messageValue = sanitizer.sanitize(req.body.message);
