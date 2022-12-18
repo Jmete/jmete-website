8bf2c0f714aadd11abdc1fbb83e8d165eda53419
James Mete <jmete3@gmail.com>
2022-12-18 23:20:56 +0000

    Added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import partytown from "@astrojs/partytown";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(), 
-    partytown({
-      config: {
-        forward: ["datalayer.push"],
-      }
-    }),
-  ]
+  site: 'https://jamesmete.com',
+  integrations: [tailwind(), partytown({
+    config: {
+      forward: ["datalayer.push"]
+    }
+  }), sitemap()]
 });
